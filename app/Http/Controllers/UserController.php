b49c834b1af9387003118764d03890031bd274f5
amrito502 <amritobosu7@gmail.com>
2025-01-23 21:48:17 +0600

    user-registration-some-add

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,12 +8,26 @@ use Illuminate\Http\Request;
 class UserController extends Controller
 {
     public function UserRegistration(Request $request){
-        return User::create([
-             'firstName'=> $request->input('firstName'),
-             'lastName'=> $request->input('lastName'),
-             'email'=> $request->input('email'),
-             'mobile'=> $request->input('mobile'),
-             'password'=> $request->input('password'),
-         ]);
+        try{
+            User::create([
+                'firstName'=> $request->input('firstName'),
+                'lastName'=> $request->input('lastName'),
+                'email'=> $request->input('email'),
+                'mobile'=> $request->input('mobile'),
+                'password'=> $request->input('password'),
+            ]);
+
+            return response()->json([
+               'status'=>'success',
+               'message'=> 'User Registration Successfully!'
+            ]);
+        }
+        catch(\Exception $e){
+            return response()->json([
+                'status'=>'failed',
+                // 'message'=> 'Failed User Registration!'
+                'message'=> $e->getMessage()
+             ]);
+        }
      }
 }
