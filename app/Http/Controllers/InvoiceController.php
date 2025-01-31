a420957c0f43927efa37d68dfccb99c635c49893
amrito502 <amritobosu7@gmail.com>
2025-01-31 23:37:11 +0600

    invoice, report and dashboard summary generate

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,107 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
+use App\Models\Customer;
 use Illuminate\Http\Request;
+use App\Models\InvoiceProduct;
+use Illuminate\Support\Facades\DB;
 
 class InvoiceController extends Controller
 {
-    //
+    function InvoicePage(){
+        return view('pages.dashboard.invoice-page');
+    }
+
+    function SalePage(){
+        return view('pages.dashboard.sale-page');
+    }
+
+    public function invoiceCreate(Request $request){
+        DB::beginTransaction();
+        try{
+            $user_id = $request->header('id');
+            $total = $request->input('total');
+            $discount = $request->input('discount');
+            $vat = $request->input('vat');
+            $payable = $request->input('payable');
+
+            $customer_id = $request->input('customer_id');
+
+            $invoice = Invoice::create([
+                'total' => $total,
+                'discount' => $discount,
+                'vat' => $vat,
+                'payable' => $payable,
+                'customer_id' => $customer_id,
+                'user_id' => $user_id
+            ]);
+
+            $invoiceID = $invoice->id;
+
+            $products = $request->input('products');
+
+            foreach($products as $product){
+                InvoiceProduct::create([
+                    'invoice_id' => $invoiceID,
+                    'user_id' => $user_id,
+                    'product_id' => $product['product_id'],
+                    'qty' => $product['qty'],
+                    'sale_price' => $product['sale_price'],
+                ]);
+            }
+
+            DB::commit();
+            return 1;
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            return 0;
+        }
+    }
+
+
+    public function invoiceSelect(Request $request){
+        $user_id = $request->header('id');
+        $invoices = Invoice::where('user_id',$user_id)->with('customer')->get();
+        return $invoices;
+    }
+
+
+    public function InvoiceDetails(Request $request){
+        $user_id = $request->header('id');
+        $customerDetails = Customer::where('user_id',$user_id)->where('id',$request->input('cus_id'))->first();
+        $invoiceTotal = Invoice::where('user_id',$user_id)->where('id',$request->input('inv_id'))->first();
+        $invoiceProduct = InvoiceProduct::where('invoice_id',$request->input('inv_id'))->where('user_id',$user_id)->get();
+
+        return array(
+            'customer' => $customerDetails,
+            'invoice' => $invoiceTotal,
+            'product' => $invoiceProduct
+        );
+    }
+
+    public function invoiceDelete(Request $request){
+        DB::beginTransaction();
+        try{
+           $user_id = $request->header('id');
+           InvoiceProduct::where('invoice_id',$request->input('inv_id'))->where('user_id',$user_id)->delete();
+           Invoice::where('id',$request->input('inv_id'))->delete();
+           DB::commit();
+           return 1;
+        }
+        catch(\Exception $e){
+            DB::rollBack();
+            return 0;
+        }
+    }
+
+
+
+
+
+
+
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ReportController;
 use App\Http\Controllers\InvoiceController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\CategoryController;
@@ -57,7 +58,7 @@ Route::get('/customerPage',[CustomerController::class,'CustomerPage'])->middlewa
 Route::get('/productPage',[ProductController::class,'ProductPage'])->middleware([TokenVerificationMiddleware::class]);
 Route::get('/invoicePage',[InvoiceController::class,'InvoicePage'])->middleware([TokenVerificationMiddleware::class]);
 Route::get('/salePage',[InvoiceController::class,'SalePage'])->middleware([TokenVerificationMiddleware::class]);
-// Route::get('/reportPage',[ReportController::class,'ReportPage'])->middleware([TokenVerificationMiddleware::class]);
+Route::get('/reportPage',[ReportController::class,'ReportPage'])->middleware([TokenVerificationMiddleware::class]);
 
 
 // ==========category api================
@@ -83,4 +84,14 @@ Route::post("/update-product",[ProductController::class,'UpdateProduct'])->middl
 Route::get("/list-product",[ProductController::class,'ProductList'])->middleware([TokenVerificationMiddleware::class]);
 Route::post("/product-by-id",[ProductController::class,'ProductByID'])->middleware([TokenVerificationMiddleware::class]);
 
+
+// Invoice
+Route::post("/invoice-create",[InvoiceController::class,'invoiceCreate'])->middleware([TokenVerificationMiddleware::class]);
+Route::get("/invoice-select",[InvoiceController::class,'invoiceSelect'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/invoice-details",[InvoiceController::class,'InvoiceDetails'])->middleware([TokenVerificationMiddleware::class]);
+Route::post("/invoice-delete",[InvoiceController::class,'invoiceDelete'])->middleware([TokenVerificationMiddleware::class]);
+
+// SUMMARY & Report
+Route::get("/summary",[DashboardController::class,'Summary'])->middleware([TokenVerificationMiddleware::class]);
+Route::get("/sales-report/{FormDate}/{ToDate}",[ReportController::class,'SalesReport'])->middleware([TokenVerificationMiddleware::class]);
 // =========================
